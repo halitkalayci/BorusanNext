eb3a48843177b93d41adf5a586fde50358da078a
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2024-07-22 11:31:34 +0300

    suspense

--- src/borusannext-client/src/App.tsx
+++ src/borusannext-client/src/App.tsx
@@ -1,23 +1,32 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import Login from "./pages/Login/Login";
 import MainLayout from "./layouts/MainLayout/MainLayout";
 import {mainLayoutRoutes, noLayoutRoutes} from "./routes/routes";
+import {Suspense} from "react";
+import {Spinner} from "flowbite-react";
 
 const App = () => {
 	return (
 		<>
 			<BrowserRouter>
-				<Routes>
-					{mainLayoutRoutes.map(route => (
-						<Route
-							path={route.path}
-							element={<MainLayout>{route.element}</MainLayout>}
-						/>
-					))}
-					{noLayoutRoutes.map(route => (
-						<Route path={route.path} element={route.element} />
-					))}
-				</Routes>
+				<Suspense
+					fallback={
+						<div className="flex h-[100vh] w-full justify-center items-center">
+							<Spinner />
+						</div>
+					}
+				>
+					<Routes>
+						{mainLayoutRoutes.map(route => (
+							<Route
+								path={route.path}
+								element={<MainLayout>{route.element}</MainLayout>}
+							/>
+						))}
+						{noLayoutRoutes.map(route => (
+							<Route path={route.path} element={route.element} />
+						))}
+					</Routes>
+				</Suspense>
 			</BrowserRouter>
 		</>
 	);
--- src/borusannext-client/src/pages/Homepage/Homepage.tsx
+++ src/borusannext-client/src/pages/Homepage/Homepage.tsx
@@ -1,13 +1,9 @@
-import {Button} from "flowbite-react";
-
 type Props = {};
 
 const Homepage = (props: Props) => {
 	return (
 		<div className="flex items-center justify-center mt-5">
-			<div className="grid grid-cols-12">
-				<Button>Tıkla</Button>
-			</div>
+			<div className="grid grid-cols-12"></div>
 		</div>
 	);
 };
