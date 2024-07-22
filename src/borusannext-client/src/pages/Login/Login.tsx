dff14497e1ca7346569b2376bae8277dc1a7bb78
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2024-07-22 11:56:51 +0300

    formik & yup

--- src/borusannext-client/src/pages/Login/Login.tsx
+++ src/borusannext-client/src/pages/Login/Login.tsx
@@ -1,34 +1,71 @@
 import {Button, Card, Label, TextInput} from "flowbite-react";
+import {ErrorMessage, Field, Form, Formik} from "formik";
 import React from "react";
+import * as yup from "yup";
 
 type Props = {};
 
+type LoginFormValues = {
+	email: string;
+	password: string;
+};
+
 const Login = (props: Props) => {
+	const validationSchema = yup.object({
+		email: yup.string().required().email(),
+		password: yup.string().required().min(3).max(10), //.matches(),
+	});
+
+	const initialValues: LoginFormValues = {
+		email: "",
+		password: "",
+	};
+
 	return (
 		<div className="flex w-full h-[100vh] justify-center items-center">
 			<Card className="p-10">
-				<form className="flex flex-col w-full">
-					<div>
-						<div className="mb-2 block">
-							<Label htmlFor="email1" value="E-posta" />
+				<h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+					Giriş Yap
+				</h5>
+				<Formik
+					initialValues={initialValues}
+					validationSchema={validationSchema}
+					onSubmit={e => console.log(e)}
+				>
+					<Form className="flex flex-col w-full">
+						<div>
+							<div className="mb-2 block">
+								<Label htmlFor="email1" value="E-posta" />
+							</div>
+							<Field
+								id="email1"
+								type="email"
+								placeholder="name@flowbite.com"
+								required
+								name="email"
+							/>
+							<ErrorMessage
+								name="email"
+								component={"div"}
+								className="text-red-500"
+							/>
 						</div>
-						<TextInput
-							id="email1"
-							type="email"
-							placeholder="name@flowbite.com"
-							required
-						/>
-					</div>
-					<div>
-						<div className="mb-2 block">
-							<Label htmlFor="password1" value="Şifre" />
+						<div>
+							<div className="mb-2 block">
+								<Label htmlFor="password1" value="Şifre" />
+							</div>
+							<Field id="password1" type="password" required name="password" />
+							<ErrorMessage
+								name="password"
+								component={"div"}
+								className="text-red-500"
+							/>
 						</div>
-						<TextInput id="password1" type="password" required />
-					</div>
-					<Button type="submit" className="mt-3">
-						Submit
-					</Button>
-				</form>
+						<Button type="submit" className="mt-3">
+							Submit
+						</Button>
+					</Form>
+				</Formik>
 			</Card>
 		</div>
 	);
