24c40bd6223e046d6ec09ac6b24e6fefea8d1eac
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2024-07-23 10:44:09 +0300

    token control

--- src/borusannext-client/src/pages/Homepage/Homepage.tsx
+++ src/borusannext-client/src/pages/Homepage/Homepage.tsx
@@ -1,6 +1,21 @@
+import {useEffect} from "react";
+import {BrandsApi} from "../../api";
+
 type Props = {};
 
 const Homepage = (props: Props) => {
+	useEffect(() => {
+		fetchBrands();
+	}, []);
+
+	const fetchBrands = async () => {
+		const brandApi = new BrandsApi();
+
+		const brands = await brandApi.apiBrandsGet(0, 50);
+
+		console.log(brands);
+	};
+
 	return (
 		<div className="flex items-center justify-center mt-5">
 			<div className="grid grid-cols-12"></div>
