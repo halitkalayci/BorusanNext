641612c89dfb890e0c35f155fa6cebfdc971cbde
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2024-07-24 10:05:09 +0300

    otp login

---
+++ src/borusannext-client/src/constants/authenticatorTypes.ts
@@ -0,0 +1,6 @@
+export const AUTHENTICATOR_TYPES = {
+	NONE: 0,
+	EMAIL: 1,
+	OTP: 2,
+	SMS: 3,
+};
--- src/borusannext-client/src/pages/Login/Login.tsx
+++ src/borusannext-client/src/pages/Login/Login.tsx
@@ -1,29 +1,35 @@
-import {Button, Card, Label, TextInput} from "flowbite-react";
+import {Button, Card, Label, Modal, TextInput} from "flowbite-react";
 import {ErrorMessage, Field, Form, Formik} from "formik";
-import React from "react";
+import React, {useState} from "react";
 import * as yup from "yup";
 import CustomInput from "../../components/CustomInput/CustomInput";
 import "./Login.css";
 import {AuthApi} from "../../api";
 import {useNavigate} from "react-router-dom";
+import {AUTHENTICATOR_TYPES} from "../../constants/authenticatorTypes";
 type Props = {};
 
 type LoginFormValues = {
 	email: string;
 	password: string;
+	authenticatorCode?: string | null;
 };
 
 const Login = (props: Props) => {
 	const navigate = useNavigate(); // hooks
 
+	const [openModal, setOpenModal] = useState(false);
+	const [modalText, setModalText] = useState("");
+
 	const validationSchema = yup.object({
-		email: yup.string(), //required().email(),
+		email: yup.string().required().email(),
 		password: yup.string().required().min(3).max(10), //.matches(),
 	});
 
 	const initialValues: LoginFormValues = {
 		email: "",
 		password: "",
+		authenticatorCode: null,
 	};
 
 	const submit = async (formValues: LoginFormValues) => {
@@ -31,12 +37,30 @@ const Login = (props: Props) => {
 
 		const loginResponse = await authApi.apiAuthLoginPost({
 			...formValues,
-			authenticatorCode: "",
 		});
 
-		localStorage.setItem("token", loginResponse.data.accessToken?.token!);
+		if (loginResponse.data.accessToken) {
+			localStorage.setItem("token", loginResponse.data.accessToken?.token!);
 
-		navigate("/");
+			navigate("/");
+		} else {
+			switch (loginResponse.data.requiredAuthenticatorType) {
+				case AUTHENTICATOR_TYPES.OTP:
+					setModalText(
+						"Lütfen otp uygulamanızdaki tek kullanımlık şifreyi giriniz.",
+					);
+					setOpenModal(true);
+					break;
+				case AUTHENTICATOR_TYPES.EMAIL:
+					setModalText(
+						"Lütfen e-posta adresinize gönderilen tek kullanımlık şifreyi giriniz.",
+					);
+					setOpenModal(true);
+					break;
+				default:
+					break;
+			}
+		}
 	};
 	//TODO: Register sayfası tasarlanması.
 	return (
@@ -50,26 +74,59 @@ const Login = (props: Props) => {
 					validationSchema={validationSchema}
 					onSubmit={e => submit(e)}
 				>
-					<Form className="flex flex-col w-full">
-						<Field
-							label="E-posta"
-							id="email"
-							placeholder="user@borusan.com"
-							name="email"
-							type="text"
-							component={CustomInput}
-						/>
-						<Field
-							label="Şifre"
-							id="password"
-							name="password"
-							type="password"
-							component={CustomInput}
-						/>
-						<Button type="submit" className="mt-3">
-							Submit
-						</Button>
-					</Form>
+					{e => {
+						return (
+							<Form className="flex flex-col w-full">
+								<Field
+									label="E-posta"
+									id="email"
+									placeholder="user@borusan.com"
+									name="email"
+									type="text"
+									component={CustomInput}
+								/>
+								<Field
+									label="Şifre"
+									id="password"
+									name="password"
+									type="password"
+									component={CustomInput}
+								/>
+								<Button type="submit" className="mt-3">
+									Submit
+								</Button>
+								<Modal
+									show={openModal}
+									size="md"
+									popup
+									onClose={() => setOpenModal(false)}
+								>
+									<Modal.Header />
+									<Modal.Body>
+										<div className="space-y-6">
+											<h3 className="text-xl font-medium text-gray-900 dark:text-white">
+												{modalText}
+											</h3>
+
+											<Field
+												label="Tek Kullanımlık Şifre"
+												id="authenticatorCode"
+												name="authenticatorCode"
+												type="text"
+												component={CustomInput}
+											/>
+
+											<div className="w-full">
+												<Button type="submit" onClick={e.submitForm}>
+													Submit
+												</Button>
+											</div>
+										</div>
+									</Modal.Body>
+								</Modal>
+							</Form>
+						);
+					}}
 				</Formik>
 			</Card>
 		</div>
